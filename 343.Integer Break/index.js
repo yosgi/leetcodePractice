f8a553a9a23fc5c4efc72a7821a6f539c93e5cc4
Your Name <l>
2020-10-28 22:26:05 +0800

    [add] 343

---
+++ 343.Integer Break/index.js
@@ -0,0 +1,14 @@
+var minSubArrayLen = function(s, nums) {
+  let start = 0
+  let total = 0 , min = Infinity
+  // 考察[start ... end] 区间是否 >= s 
+  for(let end = 0 ; end < nums.length ; end ++) {
+    total += nums[end]     
+    while(total >= s) {
+      let count = end - start + 1
+      min = Math.min(count,min)
+      total -= nums[start++]
+    }
+  }
+  return min === Infinity ? 0 : min
+};
